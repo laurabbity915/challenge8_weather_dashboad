7c86ed733a42589ddf6467f925a1a21c621328b0
laurabbity915 <laura_yuen915@outlook.com>
2023-02-07 22:24:11 +0000

    api call

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
-var APIkey = "d48d3c22f832571855dc4c11ed8381b0";
+const apiKey = "d48d3c22f832571855dc4c11ed8381b0";
 
-var weatherqueryURL = "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=" + APIkey
+var cityButton = ["Berlin", "Paris", "Edinburgh", "Madrid", "Birmingham", "London", "Hong Kong"];
 
+var city = $(this).attr("city name");
+
+
+//function for display the default citys button
 function dispalyCityButton() {
-    var cityButton = ["Berlin", "Paris", "Edinburgh", "Madrid", "Birmingham", "London", "Hong Kong"];
-    console.log(cityButton);
-    var city = $(this).attr("city name");
-    var geoqueryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityButton + "&limit=5&appid=";
-
+    $("citybtn").empty();
     for (var i = 0; i < cityButton.length; i++) {
-
         let btn = "";
         btn += "<div>";
         btn += "<button type='button' class='btn btn-primary countrybtn'>" + cityButton[i] + "</button>";
@@ -17,5 +16,24 @@ function dispalyCityButton() {
 
         $("#citybtn").append(btn);
     }
-
 }
+
+function getWeather(city) {
+    return $.ajax({
+        url: `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`,
+        dataType: 'json',
+    });
+}
+
+$(document).ready(function () {
+    $('#search-btn').click(function () {
+        const city = $('#search-input').val();
+        getWeather(city).then(function (weather) {
+            console.log(weather);
+            
+        // }).fail(function (error) {
+        //     console.error(error);
+        //     $('#weather-data').html('City not found');
+        });
+    });
+});
